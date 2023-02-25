8f38bd6c5e3e5e8867a047101408ddc995b324ac
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2023-02-24 23:37:32 -0300

    teste.html com uso de Ajax para mostrar as tarefas criadas

---
+++ src/scripts/mostrarTask.js
@@ -0,0 +1,19 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+function mostrarTask(){
+    $(document).ready(function() {
+      $("#botao-ajax").click(function() {
+        $.ajax({
+          url: "task.php",
+          type: "GET",
+          //data: {},
+          success: function(result) {
+            $("#resultado-ajax").html(result);
+          },
+          error: function() {
+            alert("Ocorreu um erro ao enviar a solicitação Ajax.");
+          }
+        });
+      });
+    });
+}
